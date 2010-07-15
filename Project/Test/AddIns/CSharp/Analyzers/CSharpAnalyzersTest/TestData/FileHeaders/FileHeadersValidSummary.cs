5a669779f0fbf0fe98918a414c58597079f70a2f
Jason Allor <jason.allor@microsoft.com>
2010-07-15 15:48:22 -0700

    Implemented dynamic load and reset for all Statement types

---
+++ Project/Test/AddIns/CSharp/Analyzers/CSharpAnalyzersTest/TestData/FileHeaders/FileHeadersValidSummary.cs
@@ -0,0 +1,5 @@
+// <copyright file="FileHeadersValidSummary.cs" company="Microsoft">
+//   Copyright (c) Microsoft Corporation.
+// </copyright>
+// <summary>This is the summary.</summary>
+using System;
