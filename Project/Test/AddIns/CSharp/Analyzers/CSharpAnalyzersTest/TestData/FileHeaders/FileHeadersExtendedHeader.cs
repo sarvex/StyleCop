fbf802a616c365cf3c4cc509fbfcfb5bcb53e4d6
Jason Allor <jason.allor@microsoft.com>
2010-07-21 13:39:48 -0700

    Backed out changeset: f418d1d71dd8

---
+++ Project/Test/AddIns/CSharp/Analyzers/CSharpAnalyzersTest/TestData/FileHeaders/FileHeadersExtendedHeader.cs
@@ -0,0 +1,6 @@
+//-----------------------------------------------------------------------
+// <copyright file="FileHeadersExtendedHeader.cs" company="Microsoft">
+//   Copyright (c) Microsoft Corporation.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
---
+++ Project/Test/AddIns/CSharp/Analyzers/CSharpAnalyzersTest/TestData/FileHeaders/FileHeadersExtraTags.cs
@@ -0,0 +1,6 @@
+// <copyright file="FileHeadersExtraTags.cs" company="Microsoft">
+//   Copyright (c) Microsoft Corporation.
+// </copyright>
+// <summary>This is a summary</summary>
+// <author>John Doe</author>
+using System;
