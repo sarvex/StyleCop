039c70e5a9dfcd7bdcfe9669104d5e3fcdc5bd7f
Oleg Shuruev <Oleg Shuruev@localhost>
2011-04-03 02:03:38 +0400

    More accurate fix for #6843. Removed ServiceNotification option, also slightly reworked AutoUpdater class.

---
+++ Project/Test/AddIns/CSharp/Analyzers/CSharpAnalyzersTest/TestData/BuiltInTypes/ShorthandForNullableTypes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpAnalyzersTest.TestData.BuiltInTypes
+{
+    class ShorthandForNullableTypes
+    {
+        private DateTime? d1; // valid
+
+        private Nullable<DateTime> d2; //invalid
+
+        private Nullable<int> i1; // invalid
+
+        private int? i2; // valid
+
+        private NullableX<int> nx1; // valid
+
+        public void Method(
+            Nullable<bool> b1, // invalid
+            bool? b2) // valid
+        {
+            Nullable<bool> c1 = null; // invalid
+            bool? c2 = null; // valid
+        }
+
+        public void Method2()
+        {
+            var a = typeof(Nullable<>); // valid
+        }
+    }
+}
