ff3a5d41b6b9ae9dacb242f4b87f80a41b59b1f5
Andy Reeves <Andy Reeves@localhost>
2011-05-18 18:09:36 +0100

    Report more information for SA1649 if the typename in the header needs updating.

---
+++ Project/Src/AddIns/CSharp/Parser/Expressions/DefaultValueExpression.cs
@@ -0,0 +1,91 @@
+//-----------------------------------------------------------------------
+// <copyright file="DefaultValueExpression.cs">
+//   MS-PL
+// </copyright>
+// <license>
+//   This source code is subject to terms and conditions of the Microsoft 
+//   Public License. A copy of the license can be found in the License.html 
+//   file at the root of this distribution. If you cannot locate the  
+//   Microsoft Public License, please send an email to dlr@microsoft.com. 
+//   By using this source code in any fashion, you are agreeing to be bound 
+//   by the terms of the Microsoft Public License. You must not remove this 
+//   notice, or any other, from this software.
+// </license>
+//-----------------------------------------------------------------------
+namespace StyleCop.CSharp
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// An expression representing a default value operation.
+    /// </summary>
+    /// <subcategory>expression</subcategory>
+    public sealed class DefaultValueExpression : Expression, ICodePart
+    {
+        /// <summary>
+        /// The parent of the parameter.
+        /// </summary>
+        private readonly Reference<ICodePart> parent;
+
+        #region Private Fields
+
+        /// <summary>
+        /// The type to obtain the default value of.
+        /// </summary>
+        private TypeToken type;
+        
+        #endregion Private Fields
+
+        #region Internal Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the DefaultValueExpression class.
+        /// </summary>
+        /// <param name="tokens">The list of tokens that form the expression.</param>
+        /// <param name="type">The type to obtain the default value of.</param>
+        /// <param name="parent">The parent reference of this expression.</param>
+        internal DefaultValueExpression(CsTokenList tokens, LiteralExpression type, Reference<ICodePart> parent)
+            : base(ExpressionType.DefaultValue, tokens)
+        {
+            Param.AssertNotNull(tokens, "tokens");
+            Param.AssertNotNull(type, "type");
+
+            this.type = CodeParser.ExtractTypeTokenFromLiteralExpression(type);
+            this.AddExpression(type);
+
+            this.parent = parent;
+        }
+        
+        /// <summary>
+        /// Gets the actual parent codepartof this expression. Normally a Parameter.
+        /// </summary>
+        ICodePart ICodePart.Parent
+        {
+            get
+            {
+                return this.parent.Target;
+            }
+        }
+   
+        #endregion Internal Constructors
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the type to obtain the default value of.
+        /// </summary>
+        [SuppressMessage(
+            "Microsoft.Naming", 
+            "CA1721:PropertyNamesShouldNotMatchGetMethods",
+            Justification = "API has already been published and should not be changed.")]
+        public TypeToken Type
+        {
+            get
+            {
+                return this.type;
+            }
+        }
+        
+        #endregion Public Properties
+    }
+}
---
+++ Project/Test/AddIns/CSharp/Analyzers/CSharpAnalyzersTest/TestData/Spacing/OpenParenthesis.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpAnalyzersTest.TestData.Spacing
+{
+    class OpenParenthesis
+    {
+        public void M(Action a)
+        {
+            M(a: () => Console.WriteLine());
+        }
+
+        public void Method1()
+        {
+            this.Foo1(param: (object)null);
+            this.Foo2(param: (int x) => { return x * x; });
+            this.Foo3(param: () => { });
+        }
+
+        public void Foo1(object param)
+        {
+        }
+
+        private void Foo2(Func<int, int> param)
+        {
+        }
+
+        private void Foo3(Action param)
+        {
+        }
+   }
+}
